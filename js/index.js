c406d40566bbf64eb3ee0de522868e40f008a614
kso <so555art@gmail.com>
2026-04-15 17:23:20 +0900

    fifth

--- js/index.js
+++ js/index.js
@@ -261,9 +261,10 @@ $(function () {
 
   // container -> con03
 
-  let con03 = $(".con03").offset().top - 700;
   $(window).on("scroll", function () {
     let scroll = $(this).scrollTop();
+    let w = $(window).width();
+    let con03 = $(".con03").offset().top - 700;
 
     // console.log(con3);
 
@@ -279,14 +280,11 @@ $(function () {
         .css({ transform: "translateX(-10rem)", opacity: "0" });
     }
 
-    //
+    // con03 오른쪽 사진
 
     if (w < 840) {
       if (scroll >= con03 + 300) {
-        $("#container .con03 .right ul li:nth-child(odd)")
-          .stop()
-          .css({ transform: "translateY(0rem)", opacity: "1" });
-        $("#container .con03 .right ul li:nth-child(even)")
+        $("#container .con03 .right ul li")
           .stop()
           .css({ transform: "translateY(0rem)", opacity: "1" });
 
@@ -297,10 +295,7 @@ $(function () {
       } else {
         $("#container .con03 .right ul li:nth-child(odd)")
           .stop()
-          .css({ transform: "translateY(10rem)", opacity: "0" });
-        $("#container .con03 .right ul li:nth-child(even)")
-          .stop()
-          .css({ transform: "translateY(20rem)", opacity: "0" });
+          .css({ transform: "translateY(3rem)", opacity: "0" });
 
         $("#container .con03 .right ul li:nth-child(1) .pic_wrap").stop().css({
           transform: "translate(10px, 10px)",
